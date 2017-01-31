0a496c779e38eb9b9475b12709d4b7363ac44234
Jose Muñoz <josemlozano6@gmail.com>
2017-01-31 15:38:00 +0100

    First commit

---
+++ android/src/com/example/jose/spaceinvaders/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.example.jose.spaceinvaders;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.example.jose.spaceinvaders.SpaceInvaders;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpaceInvaders(), config);
+	}
+}
